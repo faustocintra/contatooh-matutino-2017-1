8ba34848cdf1ae8125a9f57a7c7a330e7d9d4936
fgcintra <fausto@fgcintra.pro.br>
2017-05-02 09:05:31 -0300

    Alterado o restante do controller contato.js para funcionar com o MongoDB

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -18,12 +18,62 @@ module.exports = function(app) {
    };
    
    controller.obtemContato = function(req, res) {
+      Contato.findById(req.params.id).exec().then(
+         function(contato) {
+            if(! contato) { // Não encontrou o id
+               throw new Error('Contato não encontrado');
+            }
+            res.json(contato);
+         },
+         function(erro) {
+            console.log(erro);
+            res.status(404).json(erro);
+         }
+      );
    };
 
    controller.removeContato = function(req, res) {
+      Contato.remove({_id: req.params.id}).exec().then(
+         function() {
+            // HTTP 204: OK, sem conteúdo posterior
+            res.status(204).end();
+         },
+         function(erro) {
+            return console.error(erro);
+         }
+      );
    };
 
    controller.salvaContato = function(req, res) {
+      if(req.body._id) { // Atualização
+
+         Contato.findByIdAndUpdate(req.body._id, req.body)
+            .exec().then(
+               function(contato) {
+                  res.json(contato);
+               },
+               function(erro) {
+                  console.error(erro);
+                  // HTTP 500: erro interno do servidor                  
+                  res.status(500).json(erro);
+               }
+            );
+
+      }
+      else { // Inserção
+
+         Contato.create(req.body).then(
+            function(contato) {
+               // HTTP 201: criado
+               res.status(201).json(contato);
+            },
+            function(erro) {
+               console.log(erro);
+               res.status(500).json(erro);
+            }
+         ); 
+
+      }
    };
    
    return controller;
